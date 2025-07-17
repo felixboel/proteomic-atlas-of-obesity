2de11f91693432ac9b2019821a82731a3a0f2014
felixboel <147402201+felixboel@users.noreply.github.com>
2025-07-17 18:39:40 +0200

    App launcher

---
+++ app.R
@@ -0,0 +1,17 @@
+# List of required packages
+packages <- c("shiny", "shinymanager", "shinyTree", "ggplot2", "plotly", "dplyr", "tidyr", "readr")
+
+# Install any missing packages
+install_if_missing <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, repos = "https://cloud.r-project.org/")
+  }
+}
+invisible(lapply(packages, install_if_missing))
+
+# Source app file
+source("sub_folder/proteomics_explorer.R")
+
+# Run the shiny app
+app <- shinyApp(ui = ui, server = server)
+runApp(app, launch.browser = TRUE)
